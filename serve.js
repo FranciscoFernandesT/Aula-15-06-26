087e2983bff930892af8bf2b19705e9779b7ef93
FranciscoFernandesT <francisco32p1real@gmail.com>
2026-06-16 20:59:28 -0300

    Add files via upload

--- serve.js
+++ serve.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/uno', (req, res) => {
     res.send('Hello World');
 });
 
-app.post('/dos', (req, res) => {
+app.get('/dos', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/tres/:nome', (req, res) => {
+    const dados = req.body;
+    console.log(dados);
+    res.send(req.params.nome);
+    console.log(req.body)
 });
 
 app.listen(3000, () => {
